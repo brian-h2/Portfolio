6d9a4942467d89207c3368483450fb37e9a522bf
brian-h2 <brianheredia2004@gmail.com>
2024-06-09 10:39:48 -0300

    Agregado de un nuevo proyecto

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,19 @@
 import './header.css'
+import { useState } from 'react'
 import scriptIcon from '../../assets/icons/script.svg' 
 import hamburIcon from '../../assets/icons/menu-hambur.svg' 
 
 
 export function Header() {
+
+    const [estado,setEstado] = useState(false);
+
+    //El estado en un principio es false, al darle click se volveria true y mostraria las opciones
+
+    const handleMenuClick = () => {
+        
+    }
+
     return (
         <div className="header">
             <div className="header-name">
@@ -12,8 +22,8 @@ export function Header() {
             </div>
               
             <div className="header-options">
-                <img className="menu" src={hamburIcon} alt="hambur-icon" id="hambur-icon"/>
-                <ul>
+                <img className="menu" src={hamburIcon}  alt="hambur-icon"  id="hambur-icon"/>
+                <ul className="header-options-links">
                     <a>Home</a>
                     <a>Summary</a>
                     <a>Proyects</a>
--- src/components/proyects/proyects.jsx
+++ src/components/proyects/proyects.jsx
@@ -3,11 +3,15 @@ import proyectOne from '../../assets/images/1.png';
 import proyectTwo from '../../assets/images/2.png';
 import proyectThree from '../../assets/images/3.png';
 import proyectFour from '../../assets/images/5.png';
+import proyectFive from '../../assets/images/6.jpg';
 import iconCss from '../../assets/icons/icons-lenguajes/css.svg';
 import iconHtml from '../../assets/icons/icons-lenguajes/html.svg';
 import iconSaas from '../../assets/icons/icons-lenguajes/sass.svg';
 import iconJs from '../../assets/icons/icons-lenguajes/js.svg';
 import iconReact from '../../assets/icons/icons-lenguajes/react.svg';
+import iconCsharp from '../../assets/icons/icons-lenguajes/c-sharp.svg';
+import iconNet from '../../assets/icons/icons-lenguajes/net.svg';
+import iconGit from '../../assets/icons/icons-lenguajes/git.svg';
 import webIcon from '../../assets/icons/web.svg';
 import iconNode from '../../assets/icons/icons-lenguajes/node.svg';
 import githubIcon from '../../assets/icons/github.svg';
@@ -65,22 +69,38 @@ export function Proyects() {
                             </div>
                     </div>
                     <div className="proyect">
-                    <img src={proyectFour}/>
-                            <div className="proyect-resum">
-                            <p>Eccomerce</p>
-                                <a target="blank"href='https://github.com/brian-h2/Proyecto-final-JS-CoderHouse'><img src={githubIcon} alt=""/></a>
-                                <a href="https://github.com/brian-h2/Proyecto-final-JS-CoderHouse" target='blank'><img src={webIcon} alt="" srcset=""/></a>
-                            </div>
-                            
-                            <div className='proyect-icons'>
-                                <img src={iconCss}/>
-                                <img src={iconHtml}/>
-                                <img src={iconJs}/>
-                                <img src={iconNode}/>
-                               
-                            </div>
+                        <img src={proyectFour}/>
+                                <div className="proyect-resum">
+                                <p>Eccomerce</p>
+                                    <a target="blank"href='https://github.com/brian-h2/Proyecto-final-JS-CoderHouse'><img src={githubIcon} alt=""/></a>
+                                    <a href="https://github.com/brian-h2/Proyecto-final-JS-CoderHouse" target='blank'><img src={webIcon} alt="" srcset=""/></a>
+                                </div>
+                                
+                                <div className='proyect-icons'>
+                                    <img src={iconCss}/>
+                                    <img src={iconHtml}/>
+                                    <img src={iconJs}/>
+                                    <img src={iconNode}/>
+                                
+                                </div>
                     </div>
+                    <div className="proyect">
+                        <img src={proyectFive}/>
+                                <div className="proyect-resum">
+                                <p>Administration System</p>
+                                    <a target="blank"href='https://github.com/brian-h2/pryIntegrador'><img src={githubIcon} alt=""/></a>
+                                </div>
+                                
+                                <div className='proyect-icons'>
+                                    <img src={iconCsharp}/>
+                                    <img src={iconNet}/>
+                                    <img src={iconGit}/>
+                                
+                                </div>
+                        </div>
+                    </div>
+                   
                 </div>
-            </div>
+            
     )
 }
