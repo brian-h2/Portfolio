6108cf0cd0d168c558bfdf7e389b62cb90a5d622
brian-h2 <brianheredia200309@gmail.com>
2024-07-09 10:43:42 -0300

    Enrutado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Header } from './components/header/header'
 import { Proyects } from './components/proyects/proyects'
 import Skills from './components/skills/skills';
@@ -8,18 +8,18 @@ import { Summary } from './components/summary/summary'
 
 function App() {
  
-
   return (
-    <div className="app">
-     
-        <Header className='header'/>
-        <Summary/>
-        <Proyects/>
-        <Skills/>
-      
-     
-      {/* <Contact/>  */}
-     
+    <div className='app'>
+      <Header />
+      <Summary />
+      <Proyects />
+      <Skills/>
+      <Routes>
+        <Route path="/" component={Header} />
+        <Route path="/Summary" component={<Summary/>} />
+        <Route path="/proyects/proyects" component={<Proyects/>} />
+        <Route path="/Skills" component={<Skills/>} />
+      </Routes>
     </div>
   )
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,52 +1,50 @@
 import './header.css'
-import { useRef, useState } from 'react'
-import { useTranslation } from 'react-i18next'
-import scriptIcon from '../../assets/icons/script.svg' 
-import hamburIcon from '../../assets/icons/menu-hambur.svg' 
+import { Link } from 'react-router-dom';
+import { useRef } from 'react';
+import { useTranslation } from 'react-i18next';
+import scriptIcon from '../../assets/icons/script.svg'; 
+import hamburIcon from '../../assets/icons/menu-hambur.svg'; 
 import imageUsa from '../../assets/images/flags/usa.jpg';
 import imageSpa from '../../assets/images/flags/spain.png';
 
-
-
 export function Header() {
-
-    const [t, i18n] = useTranslation("global");
+    const { t, i18n } = useTranslation("global");
 
     const menuRef = useRef();
     const menuHambur = useRef();
 
     const handleMenuClick = () => {
-        menuHambur.current.classList.toggle('active')
-        menuRef.current.classList.toggle('active')
-    }
+        menuHambur.current.classList.toggle('active');
+        menuRef.current.classList.toggle('active');
+    };
 
     return (
         <div className="header">
             <div className="header-name">
                 <h1>Brian</h1>
-                <img src={scriptIcon} alt="script-icon"/>
+                <img src={scriptIcon} alt="script-icon" />
             </div>
               
             <div className="header-options">
-                <img className="menu" ref={menuHambur} src={hamburIcon} onClick={handleMenuClick} alt="hambur-icon"  id="hambur-icon"/>
+                <img className="menu" ref={menuHambur} src={hamburIcon} onClick={handleMenuClick} alt="hambur-icon" id="hambur-icon" />
                 <div className='header-options-list'>
                     <div className='header-idioma'>
-                        <button onClick={() => i18n.changeLanguage("es")}><img src={imageSpa}/></button>
-                        <button onClick={() => i18n.changeLanguage("en")}><img src={imageUsa}/></button>
+                        <button onClick={() => i18n.changeLanguage("es")}><img src={imageSpa} alt="Spanish flag" /></button>
+                        <button onClick={() => i18n.changeLanguage("en")}><img src={imageUsa} alt="USA flag" /></button>
                     </div>
                     
                     <ul className="header-options-links" ref={menuRef}>
                         <div className='header-idioma-buton'>
-                            <button onClick={() => i18n.changeLanguage("es")}><img src={imageSpa}/></button>
-                            <button onClick={() => i18n.changeLanguage("en")}><img src={imageUsa}/></button>
+                            <button onClick={() => i18n.changeLanguage("es")}><img src={imageSpa} alt="Spanish flag" /></button>
+                            <button onClick={() => i18n.changeLanguage("en")}><img src={imageUsa} alt="USA flag" /></button>
                         </div>
-                        <a>{t("navbar.navbar-element-1")}</a>
-                        <a>{t("navbar.navbar-element-2")}</a>
-                        <a>{t("navbar.navbar-element-3")}</a>
-                        <a>{t("navbar.navbar-element-4")}</a>
+                        <li><Link to="/home">{t("navbar.navbar-element-1")}</Link></li>
+                        <li><Link to="/Summary">{t("navbar.navbar-element-2")}</Link></li>
+                        <li><Link to="/Proyects">{t("navbar.navbar-element-3")}</Link></li>
+                        <li><Link to="/Skills">{t("navbar.navbar-element-4")}</Link></li>
                     </ul>
                 </div>
             </div>
         </div>
-    )
+    );
 }
